580006cef3699d8a53c42714bc8cf8981cd17308
Shantanu Gupta <shans95g@gmail.com>
2015-02-15 19:20:50 +0530

    [google-api] Fetch user lists
    	modified:   app.js
    	modified:   index.html

--- app.js
+++ app.js
@@ -35,12 +35,12 @@ template.onMenuSelect = function(e, detail, sender) {
 		if (idx != sel) {
 			document.querySelector('#Tab' + idx).style.display = 'none';
 		}
-	} while(++idx < 7);
+	} while(++idx < 8);
 
 	document.querySelector('#Tab' + sel).style.display = 'block';
 
 	// This needs logged in google account
-	if ((sel == 4 || sel == 3) && !this.isAuthenticated) {
+	if ((sel == 5 || sel == 4 || sel == 2) && !this.isAuthenticated) {
 		var prompt = document.querySelector('#loginpd');
 		if (prompt.opened != true) {
 			prompt.toggle();
@@ -59,13 +59,57 @@ template.onMenuSelect = function(e, detail, sender) {
 	}
 }
 
-template.onSigninSuccess = function(e, detail, sender) {
-	this.isAuthenticated = true;
-
-	if (template.users) {
+function populateTab(tid, playlistId, pageToken) {
+	if (tid == 2) {
+		$.each(playlistId, function(index, item) {
+			var videoId;
+			if (item.contentDetails.upload)
+				videoId = item.contentDetails.upload.videoId;
+			if (item.contentDetails.like)
+				videoId = item.contentDetails.like.resourceId.videoId;
+			if (item.contentDetails.recommendation)
+				videoId = item.contentDetails.recommendation.resourceId.videoId;
+			if (videoId) {
+				var requestVideo = gapi.client.youtube.videos.list({
+					id: videoId,
+					part: 'snippet'
+				});
+				requestVideo.execute(function(resp) {
+					displayResult(2, resp.result.items[0].snippet.title, videoId);
+				});
+			}
+		});
 		return;
 	}
 
+	$('#video-container'+tid).html('');
+	var requestOptions = {
+		playlistId: playlistId,
+		part: 'snippet',
+		maxResults: 10
+	};
+	if (pageToken) {
+		requestOptions.pageToken = pageToken;
+	}
+	var request = gapi.client.youtube.playlistItems.list(requestOptions);
+	request.execute(function(response) {
+		var playlistItems = response.result.items;
+		if (playlistItems) {
+			$.each(playlistItems, function(index, item) {
+				displayResult(tid, item.snippet.title, item.snippet.videoId);
+			});
+		} else {
+			$('#video-container'+tid).html('You do not have any videos in this list.');
+		}
+	});
+}
+
+function displayResult(tid, title, videoId) {
+	$('#video-container'+tid).append('<p>' + title + ' - ' + videoId + '</p>');
+}
+
+
+template.onSigninSuccess = function(e, detail, sender) {
 	this.gapi = e.detail.gapi;
 
 	gapi.client.load('plus', 'v1').then(function() {
@@ -81,7 +125,34 @@ template.onSigninSuccess = function(e, detail, sender) {
 				profile: img || null
 			};
 		});
+
+		/*
+		 * My first google-API based app, got struck by a known bug already!
+		 *
+		 * API v3 Watch History returns empty erratically
+		 * https://code.google.com/p/gdata-issues/issues/detail?id=4642
+		 */
+		gapi.client.load('youtube', 'v3', function() {
+				var requestLists = gapi.client.youtube.channels.list({
+					mine: true,
+					part: 'contentDetails'
+				});
+				requestLists.execute(function(resp) {
+					populateTab(5, resp.result.items[0].contentDetails.relatedPlaylists.watchLater);
+					populateTab(4, resp.result.items[0].contentDetails.relatedPlaylists.watchHistory);
+				});
+
+				var requestFeeds = gapi.client.youtube.activities.list({
+					home: true,
+					part: 'contentDetails'
+				});
+				requestFeeds.execute(function(resp) {
+					populateTab(2, resp.result.items);
+				});
+			});
 	});
+
+	this.isAuthenticated = true;
 };
 
 template.isAuthenticated = false;
